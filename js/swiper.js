5b7a8b5d52ecbff045e97b3a789e8cbba53b3677
Silvia Cabello Martínez <silviacabellom@gmail.com>
2023-02-02 16:25:27 +0100

    text_and_picture with swiper

---
+++ js/swiper.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper('.swiper_images', {
+    direction: 'horizontal',
+    loop: true,
+  
+  
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true
+    }
+  });
